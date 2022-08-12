bca61ab1c1aab0ba3ccca591bad93ef073538295
Dmytro Melnyk <dmytro.melnyk.1992@gmail.com>
2022-08-12 19:37:35 +0300

    9 of 10 fulfilled

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const changeColorButton = document.querySelector(".change-color");
+
+changeColorButton.addEventListener("click", () => {
+  console.log("Button was clicked");
+
+  document.body.style.background = getRandomHexColor();
+  let i = 0;
+  if (i > getRandomHexColor.length - 1) {
+    i++;
+  }
+});
