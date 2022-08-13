a36207c54e8dac11f493e3926a37fe618093006c
Dmytro Melnyk <dmytro.melnyk.1992@gmail.com>
2022-08-13 21:57:51 +0300

    fixed 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,17 +1,14 @@
+const randomColor = document.querySelector(".widget");
+const span = document.querySelector(".color");
+randomColor.addEventListener("click", onButtonClick);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-const changeColorButton = document.querySelector(".change-color");
-
-const currentRandomColor = document.querySelector(".color");
-currentRandomColor.textContent = body.style.background;
-
-changeColorButton.addEventListener("click", () => {
-  console.log("Button was clicked");
-
-  document.body.style.background = getRandomHexColor();
-  let i = 0;
-  if (i > getRandomHexColor.length - 1) {
-    i++;
+function onButtonClick(event) {
+  span.textContent = getRandomHexColor();
+  if (!event.target.classList.contains("change-color")) {
+    return;
   }
-});
+  return (document.body.style.background = getRandomHexColor());
+}
